6a4b6e084373c2c5dc4112b1b05121ec57cde6c2
tingfeng-key <1770963469@qq.com>
2017-06-20 11:26:00 +0800

    first

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,20 +1,77 @@
 <!DOCTYPE html>
+<html lang="{{ config('app.locale') }}">
 <head>
-    <title>Pusher Test</title>
-    <script src="https://js.pusher.com/4.0/pusher.min.js"></script>
-    <script>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        // Enable pusher logging - don't include this in production
-        Pusher.logToConsole = true;
+    <title>欢迎您！</title>
 
-        var pusher = new Pusher('393cf83ed8f2f30f0e8c', {
-            cluster: 'ap1',
-            encrypted: true
-        });
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
-        var channel = pusher.subscribe('my-channel');
-        channel.bind('my-event', function(data) {
-            alert(data.message);
-        });
-    </script>
+    <!-- Styles -->
+    <style>
+        html, body {
+            background-color: #fff;
+            color: #636b6f;
+            font-family: 'Raleway', sans-serif;
+            font-weight: 100;
+            height: 100vh;
+            margin: 0;
+        }
+
+        .full-height {
+            height: 100vh;
+        }
+
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
+
+        .position-ref {
+            position: relative;
+        }
+
+        .top-right {
+            position: absolute;
+            right: 10px;
+            top: 18px;
+        }
+
+        .content {
+            text-align: center;
+        }
+
+        .title {
+            font-size: 84px;
+        }
+
+        .links > a {
+            color: #636b6f;
+            padding: 0 25px;
+            font-size: 12px;
+            font-weight: 600;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
+
+        .m-b-md {
+            margin-bottom: 30px;
+        }
+    </style>
 </head>
+<body>
+<div class="flex-center position-ref full-height">
+
+    <div class="content">
+        <div class="title m-b-md">
+            欢迎您！
+        </div>
+    </div>
+</div>
+</body>
+</html>
