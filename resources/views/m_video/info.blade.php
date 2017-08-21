31745e6ab73dc6e12a796674da8cd240ef688055
tingfeng-key <1770963469@qq.com>
2017-08-21 21:29:07 +0800

    first

--- resources/views/m_video/info.blade.php
+++ resources/views/m_video/info.blade.php
@@ -17,9 +17,11 @@
                 <iframe src="{{$sourceUrl}}" style="width:100%;height: 550px;"></iframe>
             </div>
             <div class="row">
+                <div class="btn-group" role="group">
                 @foreach($videos as $item)
                     <a class="btn btn-primary" href="{{route('video.info', $item->id)}}">第{{$loop->iteration}}集</a>
                 @endforeach
+                </div>
             </div>
         </div>
         <div class="panel-footer">
