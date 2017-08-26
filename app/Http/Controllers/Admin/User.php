d48ac3e7ce7053995d6cac9df952d130ec33de03
tingfeng-key <1770963469@qq.com>
2017-08-26 18:15:43 +0800

    first

---
+++ app/Http/Controllers/Admin/User.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: root
+ * Date: 2017/8/26
+ * Time: 17:08
+ */
+
+namespace App\Http\Controllers\Admin;
+
+
+class User extends Controller
+{
+    public function __construct()
+    {
+        
+    }
+    public function index()
+    {
+        
+    }
+
+
+}
