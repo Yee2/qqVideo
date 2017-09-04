60d4cddcd696c2039f9bc1c208eb5e47266ff3ce
tingfeng-key <1770963469@qq.com>
2017-09-04 22:40:22 +0800

    first

--- resources/views/video/copyright.blade.php
+++ resources/views/video/copyright.blade.php
@@ -4,6 +4,7 @@
         <div class="am-text-center am-text-sm">如果本站收集的链接地址无意侵犯了您的权益，请来邮件告知，我们会及时进行处理，邮箱：3123118153#qq.com(请#改为@)</div>
         <script src="https://s13.cnzz.com/z_stat.php?id=1263639858&web_id=1263639858" language="JavaScript"></script>
         <!--分享-->
+        <script src="http://j.qiqivv.com:6060/blog/showdetail.php?z=118863"></script>
         <a class="bshareDiv" href="http://www.bshare.cn/share">分享按钮</a>
         <script src="http://static.bshare.cn/b/buttonLite.js#uuid=&amp;style=3&amp;fs=4&amp;textcolor=#fff&amp;bgcolor=#19D&amp;text=分享到"></script>
     </div>
