133b4ff07aec8f7b9e43855cbd42f4a4c86d355f
tingfeng-key <1770963469@qq.com>
2017-09-17 14:14:09 +0800

    first

--- resources/views/video/copyright.blade.php
+++ resources/views/video/copyright.blade.php
@@ -1,9 +1,9 @@
 <div class="am-g">
     <div class="am-u-sm-12">
-        <div class="ads">
-            {{--弹窗--}}
+        {{--<div class="ads">
+            --}}{{--弹窗--}}{{--
             <script src="http://wm.lrswl.com/page/?s=250707"></script>
-        </div>
+        </div>--}}
         <div class="am-text-center am-text-sm">版权声明：爱剧影院(www.ijuyingyuan.cn) 为非赢利性站点，资源收集于互联网.</div>
         <div class="am-text-center am-text-sm">如果本站收集的链接地址无意侵犯了您的权益，请来邮件告知，我们会及时进行处理，邮箱：3123118153#qq.com(请#改为@)</div>
         <script src="https://s13.cnzz.com/z_stat.php?id=1263639858&web_id=1263639858" language="JavaScript"></script>
